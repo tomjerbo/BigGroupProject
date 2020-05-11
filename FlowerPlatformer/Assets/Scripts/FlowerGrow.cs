5bc6d0b491f38cee048b22c04f6034014fc1d1da
tomjerbo <60573821+tomjerbo@users.noreply.github.com>
2020-05-11 19:53:43 +0200

    Shitty flower

---
+++ FlowerPlatformer/Assets/Scripts/FlowerGrow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerGrow : MonoBehaviour
+{
+    public Animator anim;
+    public bool grow = false;
+    public float growSpeed = 10f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) grow = !grow;
+
+        if (grow)
+        {
+            if (anim.GetFloat("Grow") < 1)
+            {
+                anim.SetFloat("Grow", anim.GetFloat("Grow") + growSpeed * Time.deltaTime);
+            }
+            else
+            {
+                anim.SetFloat("Grow", 1f);
+            }
+        }
+        else
+        {
+            if (anim.GetFloat("Grow") >= 1)
+            {
+                anim.SetFloat("Grow", anim.GetFloat("Grow") - (growSpeed * Time.deltaTime));
+            }
+            else
+            {
+                anim.SetFloat("Grow", 0f);
+            }
+        }
+    }
+}
+
