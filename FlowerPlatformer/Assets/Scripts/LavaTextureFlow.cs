ecc1c163d1e719bb599c12baab3375260668dfa9
tomjerbo <60573821+tomjerbo@users.noreply.github.com>
2020-05-23 15:45:04 +0200

    New Test Level

---
+++ FlowerPlatformer/Assets/Scripts/LavaTextureFlow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class LavaTextureFlow : MonoBehaviour
+{
+    [SerializeField] private Material lava = default;
+    [SerializeField] private float flowSpeed = 1f;
+    [SerializeField] private Vector2 lavaUV = default;
+
+    private void Update()
+    {
+        MoveLavaUVOffset();
+        lava.mainTextureOffset = lavaUV;
+    }
+
+
+    private void MoveLavaUVOffset()
+    {
+        lavaUV.y += flowSpeed * Time.deltaTime;
+    }
+
+}
