fb50c6b16de6020f579e327979070576e2d38ea5
Jespaaah <jesper.a.nordstrom@gmail.com>
2020-06-03 15:28:19 +0200

    Revert

--- FlowerPlatformer/Assets/QuitGame.cs
+++ FlowerPlatformer/Assets/QuitGame.cs
@@ -7,11 +7,6 @@ public class QuitGame : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Quit");
-        Invoke("QGame", 4.0f);
-    }
-
-    private void QGame()
-    {
         Application.Quit();
     }
 }
